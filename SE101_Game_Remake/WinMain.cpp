a5038c5eb6b551b60076be753adefa24c5db2e8a
Lê Đăng Thương <ledangthuongsp@gmail.com>
2024-03-06 00:23:44 +0700

    Tạo thử một winmain

---
+++ SE101_Game_Remake/WinMain.cpp
@@ -0,0 +1,12 @@
+﻿#include <Windows.h>
+#include<tchar.h>
+
+HINSTANCE hInst; //tham số này định danh thể hiện của chương trình
+HWND wndHandle;
+
+int width = 640;
+int height = 480;
+
+// forward declarations
+bool InitWindow(HINSTANCE hInstance, int width, int height);
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
